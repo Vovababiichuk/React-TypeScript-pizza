c48585a78b0e6bb8c365d168ac474eecf09023c5
Vovababiichuk <babihuk888@gmail.com>
2023-10-25 16:04:57 +0300

    fix imgFormat edit form.

--- src/components/EditPizzaForm/EditPizzaFormElements.ts
+++ src/components/EditPizzaForm/EditPizzaFormElements.ts
@@ -20,7 +20,7 @@ export const MobileImageList = styled.div`
 export const BoxMyContainer = styled.div`
 	width: 100%;
 	max-width: 600px;
-	background-image: url('/img/bg-cut.jpg');
+	background-image: url('/img/bg-cut.webp');
 	background-size: cover;
 	/* background-position: center; */
 	color: #fff;
