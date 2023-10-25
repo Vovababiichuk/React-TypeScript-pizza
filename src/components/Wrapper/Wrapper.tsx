b69404d804f17f3af46ea03359c671dcda5d3cd7
Vovababiichuk <babihuk888@gmail.com>
2023-10-25 13:25:34 +0300

    fix path main img

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -5,7 +5,7 @@ import { chakra } from "@chakra-ui/react";
 			bg: "rgba(0, 0, 0, 0.7)",
 			backgroundBlendMode: "multiply",
 			minHeight: "100vh",
-			backgroundImage: "public/img/bg7.jpg",
+			backgroundImage: "/img/bg7.jpg",
 			backgroundRepeat: "no-repeat",
 			backgroundSize: "cover",
 			// backgroundPosition: "center",
